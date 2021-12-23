1a2171d0e0b5428616abc386daf256fb97e76c3a
masanori.nakano <masanori.nakano@d128.work>
2021-12-24 00:38:23 +0900

    feat: dynamicにしている意味がないので削除

--- ReactiveSwiftSample/ReactiveSwiftSample/Model/SearchModel.swift
+++ ReactiveSwiftSample/ReactiveSwiftSample/Model/SearchModel.swift
@@ -12,7 +12,7 @@ import ReactiveCocoa
 final class SearchModel: NSObject {
     private let repository = Repository()
 
-    @objc private(set) dynamic var items = [SearchResultItemModel]()
+    private(set) var items = [SearchResultItemModel]()
 
     func search(_ search: String) -> SignalProducer<Void, Error> {
         self.repository.search(search)

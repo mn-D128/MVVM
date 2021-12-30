9d5fbe0a11bd83fe8ffc76c8dac43aceee238b04
masanori.nakano <masanori.nakano@d128.work>
2021-12-30 18:09:54 +0900

    feat: 保存しない値はignoreする

--- AACSample/app/src/main/java/work/d128/aacsample/model/DetailModel.kt
+++ AACSample/app/src/main/java/work/d128/aacsample/model/DetailModel.kt
@@ -1,9 +1,11 @@
 package work.d128.aacsample.model
 
 import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class DetailModel(private val pageId: Int, val title: String): Parcelable {
+    @IgnoredOnParcel
     val url = "https://ja.wikipedia.org/?curid=" + this.pageId
 }
